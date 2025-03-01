1ede62c5e9e1490392e566fa516849006ad151d6
chingwu99 <henry88028802@hotmail.com>
2025-03-01 17:29:15 +0800

    fix: env setting

--- api/utils/getBaseUrl.ts
+++ api/utils/getBaseUrl.ts
@@ -1,6 +1,1 @@
-export const getBaseUrl = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return process.env.NEXT_PUBLIC_API_URL
-  }
-  return process.env.NEXT_PUBLIC_DEV_API_URL || 'http://localhost:3001'
-}
+export const getBaseUrl = () => process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
